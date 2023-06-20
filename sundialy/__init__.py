503855d41163f7f1be3f82915ab7ff43e54db3ce
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2023-06-20 17:02:24 +0300

    Update copyright

--- sundialy/__init__.py
+++ sundialy/__init__.py
@@ -2,4 +2,5 @@ from .analemmatic import Horizontal as AnalemmaticHorizontal
 AnalemmaticHorizontal.__name__ = "AnalemmaticHorizontal"
 
 __version__ = "0.0.1"
-__author__ = "AttackingOrDefending"
+__author__ = "Ioannis Pantidis"
+__copyright__ = "2022-2023, " + __author__
