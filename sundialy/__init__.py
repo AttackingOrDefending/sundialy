3318f1050f3c789da7d93904526c1a2d3258a9bb
Ioannis Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2024-06-08 20:39:39 +0300

    Update copyright year

--- sundialy/__init__.py
+++ sundialy/__init__.py
@@ -3,4 +3,4 @@ AnalemmaticHorizontal.__name__ = "AnalemmaticHorizontal"
 
 __version__ = "0.0.1"
 __author__ = "Ioannis Pantidis"
-__copyright__ = "2022-2023, " + __author__
+__copyright__ = "2022-2024, " + __author__
